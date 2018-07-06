e412899fcebeb6f528ece384f7dbd3028696873d
jplotnikieicz <https://dev.codedose.com/gitlab/>
2018-07-06 15:55:27 +0200

    /.../

---
+++ src/main/java/part4/Logging.java
@@ -0,0 +1,19 @@
+package part4;
+
+public class Logging {
+    public void beforeAdvice(){
+        System.out.println("Before Advice");
+    }
+
+    public void afterAdvice(){
+        System.out.println("After Advice");
+    }
+
+    public void afterReturningAdvice(Object retVal){
+        System.out.println("After Advice Executed Successfully ... Returning: " + retVal.toString() );
+    }
+
+    public void afterThrowingAdvice(IllegalArgumentException ex){
+        System.out.println("There has been an exception when executing the advice: " + ex.toString());
+    }
+}
